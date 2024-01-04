b5d16d01e5d1b1b61f475206670e86d0027bd9e8
Kimyeonho <kyh0703@nate.com>
2024-01-04 18:07:11 +0900

    Feat: learn react-flow

--- src/components/atoms/FlowNode/index.tsx
+++ src/components/atoms/FlowNode/index.tsx
@@ -25,12 +25,24 @@ const initialNodes = [
   },
   {
     id: 'node-2',
-    position: { x: 0, y: 0 },
-    data: { value: 123 },
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 0, y: 200 },
+    data: { label: 'node 2' },
+  },
+  {
+    id: 'node-3',
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 200, y: 200 },
+    data: { label: 'node 3' },
   },
 ]
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
+const initialEdges = [
+  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
+]
 
 const TextUpdaterNode = () => {
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
@@ -60,7 +72,7 @@ const nodeTypes = { textUpdater: TextUpdaterNode }
 
 const FlowNode = () => {
   const [nodes, setNodes] = useState(initialNodes)
-  const [edges, setEdges] = useState([])
+  const [edges, setEdges] = useState(initialEdges)
 
   const handleNodesChange = useCallback(
     (changes: NodeChange[]) =>
